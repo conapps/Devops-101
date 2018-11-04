75c1c0bb2f63028a6e62c10dd5bb60e3a2739dcc
Ismael <ismaa10@gmail.com>
2018-11-04 15:19:44 -0300

    Se ajusta Netconf

---
+++ Advanced Network Programmability/dia_1/codigo_dia_1/soluciones/9.py
@@ -0,0 +1,17 @@
+from ncclient import manager
+
+
+def print_capabilities(host, username, password, port='830', filter=''):
+    # escribir codigo de la funcion aqui
+    with manager.connect(host=host, port=port, username=username, password=password, hostkey_verify=False) as router:
+        for capability in router.server_capabilities:
+            if filter in capability:
+                print(capability)
+
+if __name__ == "__main__":
+    # para verificar el funcionamiento
+    HOST = 'hostname'
+    USERNAME = 'conatel'
+    PASSWORD = 'conatel'
+    filter = 'params:netconf:base'
+    print_capabilities(HOST, USERNAME, PASSWORD, filter=filter)
